d78c84cc8ca653c28dff1a5c9b9424e569f8d493
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-11 18:12:41 +0530

    Create Program04.java
    Accept number from user and return its factorial (Approach 3)

---
+++ Problems on Numbers/Program04.java
@@ -0,0 +1,36 @@
+// Accept number from user and return its factorial (Approach 3)
+
+import java.util.*;
+
+class Numbers 
+{
+    public int Factorial(int iValue) 
+    {
+        int iMult = 1;
+
+        for (int iCnt = 1; iCnt <= iValue; iCnt++) 
+        {
+            iMult = iMult * iCnt;
+        }
+        return iMult;
+    }
+}
+
+class Program04
+{
+    public static void main(String arg[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int iNo = sobj.nextInt();
+
+        Numbers nobj = new Numbers();
+
+        int iRet = nobj.Factorial(iNo);
+
+        System.out.println("Factorial is: " + iRet);
+
+        sobj.close();
+    }
+}
