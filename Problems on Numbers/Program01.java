4ccb40058124666786ac47c83b1f7db20ac187fc
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-11 18:05:41 +0530

    Create Program01.java
    Accept two numbers from user and add them

---
+++ Problems on Numbers/Program01.java
@@ -0,0 +1,20 @@
+// Addition of two numbers
+
+import java.util.*;
+
+public class Program01 {
+    public static void main(String arg[]) {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter First number: ");
+        int iNo1 = sobj.nextInt();
+
+        System.out.println("Enter Second number: ");
+        int iNo2 = sobj.nextInt();
+
+        int iAns = 0;
+        iAns = iNo1 + iNo2;
+
+        System.out.println("Addition is: " + iAns);
+    }
+}
